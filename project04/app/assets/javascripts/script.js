1078ed9e00cc1cbdb24c49916ce6eee5a08189e6
Tomas Morris <tomorris@mines.edu>
2013-02-15 19:22:40 -0700

    add a clock and some more styling

--- project04/app/assets/javascripts/script.js
+++ project04/app/assets/javascripts/script.js
@@ -1,2 +1,17 @@
 $(function(){
+    setInterval(updateClock, 1000);
 });
+
+function updateClock() {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    var mer = "AM";
+    if(hour > 12) {
+        hour -= 12;
+        mer = "PM";
+    }
+
+    $("#clock").html(sprintf("%'02u:%'02u:%'02u %s", hour, minute, second, mer));
+}
